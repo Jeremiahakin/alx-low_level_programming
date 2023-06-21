c96e0c74cf6111399c5ea646fcad2e87800b41df
Jeremiahakin <jeremiahakinniranye.com>
2023-06-21 04:19:24 -0700

    Adde the file 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
+
 /**
  * main - Entry point
  * Description: 'Prints _putchar to the stdout'
  * Return: always 0
  */
+
 int main(void)
 {
-_putcharchar('_');
-_putcharchar('p');
-_putcharchar('u');
-_putcharchar('t');
-_putcharchar('c');
-_putcharchar('h');
-_putcharchar('a');
-_putcharchar('r');
-_putcharchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
