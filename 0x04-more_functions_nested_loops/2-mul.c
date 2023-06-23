f6558ad9015fa9eb4f5fca91f61064e1b75bbdcf
Jeremiahakin <jeremiahakinniranye.com>
2023-06-22 17:32:43 -0700

    Added the file 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,17 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _isdigit - Checks for a digit
- * @c: The character to be checked
- * Return: 1 for digit character or 0 for anything else
+ * mul - multiplies two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: always 0
  */
 
-int _isdigit(int c)
+int null(int a, int b)
 
 {
-if (c >= 48 && c <= 57)
-{
-return (1);
-}
+int result = a * b;
 return (0);
 }
