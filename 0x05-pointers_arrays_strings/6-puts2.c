7fa6c303525c1ddc13de55d970b5a10563ff3276
Jeremiahakin <jeremiahakinniranye.com>
2023-06-27 16:46:23 -0700

    Added the file 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints strings
+ * @str: the used string reference
+ * Return: always 0
+ */
+void puts2(char *str)
+{
+	int z = 0;
+
+	while (str[2] != '\0')
+	{
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
+		z++;
+	}
+	_putchar('\n');
+}
