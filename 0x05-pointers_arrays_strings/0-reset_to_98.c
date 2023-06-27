79778bdc33603b2939636828f9542535983e9b5c
Jeremiahakin <jeremiahakinniranye.com>
2023-06-27 07:44:58 -0700

    Added rhe file 1-swap.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function takes int as parameter and updates the value to the pointer 98
- * do: value updated to 98
+ * reset_to_98 - takes a pointer int as parameter, points the value to 98
  * Return: always 0
  */
+
 void reset_to_98(int *n)
 {
 *n = 98;
