c47d4062cc949c9909a2714e30440219812d2088
Jeremiahakin <jeremiahakinniranye.com>
2023-06-27 18:12:09 -0700

    Didnt get tbis

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - changes a str to an int
+ * @s: the string to be checked
+ * Return: the converted int
+ */
+int _atoi(char *s)
+{
+	int i - 1;
+	unsigned int num = 0;
+	do (
+		if (*s == '-')
+	   )
+}
