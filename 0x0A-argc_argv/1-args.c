66cc8bba0bc03c8945e191864aa8a812ac6454a1
Jeremiahakin <olimidejerry30@gmail.com>
2023-10-02 18:27:58 -0700

    Added two filoes

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints each argument
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+
+
+(void)argv;
+
+printf("%d\n", argc - 1);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is my alx 0x0A-argc_argv project that teaches the concept of argc AND argv
